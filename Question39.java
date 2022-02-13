4aaf49a124ab4e198353d15a0b1ae62283d6daac
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2022-02-13 17:41:05 +0530

    Question39.java

--- Question39.java
+++ Question39.java
@@ -1,4 +1,5 @@
 /*
+Question:
 Q39.Reverse Stack
 You have been given two stacks that can store integers as the data. Out of the two given stacks, one is populated and the other one is empty. 
 You are required to write a function that reverses the populated stack using the one which is empty.
